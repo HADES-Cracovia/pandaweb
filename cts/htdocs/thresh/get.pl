37e030cd5ca647ef8f446a46bf5ad87b8fcce0c0
hadeshyp <hadeshyp>
2012-11-02 17:33:20 +0000

    *** empty log message ***

--- cts/htdocs/thresh/get.pl
+++ cts/htdocs/thresh/get.pl
@@ -10,20 +10,42 @@ use Data::Dumper;
    die("can not connect to trbnet-daemon on the $ENV{'DAQOPSERVER'}");
  }
 
-my ($board,$addr,$amount) = split('-',$ENV{'QUERY_STRING'}); 
+my ($board,$addr,$amount,$split) = split('-',$ENV{'QUERY_STRING'}); 
  
 $board = hex($board);
 $addr = hex($addr);
 $amount = 1 unless $amount;
+$split = 1 unless $split;
+
 
 if($amount != 1) {
-  my $hits = trb_register_read_mem($board,$addr,0,$amount);
-  foreach my $b (sort keys %$hits) {
-    printf ("%04x",$b);
-    for(my $c =0; $c < $amount; $c++) {
-      printf(" %d",$hits->{$b}->[$c]);
+  if ($split <= 1) {
+    my $hits = trb_register_read_mem($board,$addr,0,$amount);
+    foreach my $b (sort keys %$hits) {
+      printf ("%04x",$b);
+      for(my $c =0; $c < $amount; $c++) {
+        printf(" %d",$hits->{$b}->[$c]);
+        }
+      print "&";
       }
-    print "&";
+    }
+  else {
+    my $chunk = $amount/$split;
+    my $i = 0;
+    my @hits;
+    do {
+      $hits[$i] = trb_register_read_mem($board,$addr+$i*$chunk,0,$amount/$split);
+#       print ($board." ".($addr+$i*$chunk)." ".($amount/$split)."\n");
+      } while(++$i < $split);
+    foreach my $b (sort keys %{$hits[0]}) {
+      printf ("%04x",$b);
+      for(my $i = 0; $i < scalar @hits; $i++) {
+        for(my $c =0; $c < $chunk; $c++) {
+          printf(" %d",$hits[$i]->{$b}->[$c]);
+          }
+        }
+      print "&";
+      }
     }
   }
 else {
