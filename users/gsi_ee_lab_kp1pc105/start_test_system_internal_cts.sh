c4b2bec45d0d273f67a6eeba0307efe1e5b4a7cf
Cahit Ugur <c.ugur@gsi.de>
2013-04-23 09:19:50 +0200

    control register for data transfer limit is added - cu

--- users/gsi_ee_lab_kp1pc105/start_test_system_internal_cts.sh
+++ users/gsi_ee_lab_kp1pc105/start_test_system_internal_cts.sh
@@ -16,6 +16,7 @@ trbcmd w 0xfe48 0xc3 0x00000000 ## channel 32-63 enable
 #trbcmd w 0xfe48 0xc801 0x000f0005 ## trigger window enable & trigger window width
 #trbcmd w 0xfe48 0xc802 0x0000000f ## channel 01-31 enable
 #trbcmd w 0xfe48 0xc803 0x00000000 ## channel 32-63 enable
+#trbcmd w 0xfe48 0xc804 0x00000080 ## data transfer limit
 
 trbcmd w 0x8000 0xa137 0xfffff # set pulser #1 in CTS to 95Hz
 
