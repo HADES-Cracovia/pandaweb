5735cc41bae16a2f5c7562b767790a597636b233
Jan Michel <j.michel@gsi.de>
2013-10-17 18:26:37 +0200

    bug fix in GUI

--- web/htdocs/commands/xmlpage.pm
+++ web/htdocs/commands/xmlpage.pm
@@ -83,7 +83,9 @@ sub printJavaScripts {
       var text = e.target.getAttribute("cstr");
           text += "\\nCurrent Value: "+e.target.innerHTML+" ("+e.target.getAttribute("raw")+")\\n ";
       var newval = prompt(text,e.target.getAttribute("raw"));
-      getdataprint('../xml-db/put.pl?'+e.target.getAttribute("cstr")+'-'+newval,'returntext',false,0,refresh);
+      if (newval != null) {
+        getdataprint('../xml-db/put.pl?'+e.target.getAttribute("cstr")+'-'+newval,'returntext',false,0,refresh);
+        }
       }
     }
     
