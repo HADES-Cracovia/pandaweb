2dab2112bfa9cd3b234eba61f87ea11975a4cad1
Jan Michel <j.michel@gsi.de>
2013-10-17 19:15:41 +0200

    bugfix

--- xml-db/get.pl
+++ xml-db/get.pl
@@ -3,7 +3,7 @@ use HADES::TrbNet;
 use Storable qw(lock_retrieve);
 use feature "switch";
 
-CGI::Carp qw(fatalsToBrowser);
+use CGI::Carp qw(fatalsToBrowser);
 use if (!defined $ENV{'QUERY_STRING'}), warnings;
 use if (!defined $ENV{'QUERY_STRING'}), Pod::Usage;
 use if (!defined $ENV{'QUERY_STRING'}), Text::TabularDisplay;
