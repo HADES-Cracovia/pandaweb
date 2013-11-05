238375b1c6600d5ffca327b698643dd207ee02ac
Jan Michel <j.michel@gsi.de>
2013-11-05 15:22:28 +0100

    added some mvd / roc registers

--- web/htdocs/mvd/roc.pl
+++ web/htdocs/mvd/roc.pl
@@ -16,9 +16,15 @@ $page->{title} = "MVD Read-out Controller Register";
 $page->{link}  = "../";
 
 my @setup;
-$setup[0]->{name}    = "Status";
-$setup[0]->{cmd}     = "Mvd-0xfe4d-Status";
+$setup[0]->{name}    = "RocStatus";
+$setup[0]->{cmd}     = "Mvd-0xfe4d-RocStatus";
 $setup[0]->{period}  = 5000;
+$setup[0]->{address} = 1;
+
+$setup[1]->{name}    = "RocStatus";
+$setup[1]->{cmd}     = "Mvd-0xfe4d-RocStatus";
+$setup[1]->{period}  = -1;
+$setup[1]->{generic} = 1;
 
 xmlpage::initPage(\@setup,$page);
  
