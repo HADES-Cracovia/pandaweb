1e6c8686c1a81bbfbc48db6635bdb3c83e07d05f
Ludwig Maier <lmaier@zardoz.e12.ph.tum.de>
2013-11-18 14:47:28 +0100

    nxyter register updates

--- users/tum_e12/configure_eb.sh
+++ users/tum_e12/configure_eb.sh
@@ -4,8 +4,8 @@
 # i.e. 6C:F0:49:02:D7:45
 #trbcmd w 0x8000 0x8100 0x4902d745   # Lower 32 bits of EB MAC
 #trbcmd w 0x8000 0x8101 0x6cf0       # Bit 15..0: Higher 16 bit of EB MAC,
-trbcmd w 0x8000 0x8100 0x0c:2e:81:76 # Lower 32 bits of EB MAC
-trbcmd w 0x8000 0x8101 0x00:0e       # Bit 15..0: Higher 16 bit of EB MAC,
+trbcmd w 0x8000 0x8100 0x0c2e8176    # Lower 32 bits of EB MAC
+trbcmd w 0x8000 0x8101 0x000e        # Bit 15..0: Higher 16 bit of EB MAC,
 
 # IP Adress and Port of EB: (10.152.8.107:50000)
 #trbcmd w 0x8000 0x8102 0x0a98086b   # Destination IP
@@ -20,6 +20,6 @@ trbcmd w 0x8000 0x8105 0x0230       # Bit 15..0: Higher 16 bit of Source MAC
 
 # IP and Port of Source (10.152.8.17:50000)
 #trbcmd w 0x8000 0x8106 0x0a980811   # Source IP
-trbcmd w 0x8000 0x8102 0xc0a80111   # Destination IP
+trbcmd w 0x8000 0x8106 0xc0a80111   # Source IP
 trbcmd w 0x8000 0x8107 0xc350       # Bit 15..0: Source UDP Port 
 trbcmd w 0x8000 0x8108 0x0578       # Bit 15..0: MTU size
--- web/htdocs/nxyter/index.pl
+++ web/htdocs/nxyter/index.pl
@@ -17,43 +17,43 @@ $page->{link}  = "../";
 
 my @setup;
 $setup[0]->{name}    = "DataValid";
-$setup[0]->{cmd}     = "Nxyter-0x3800-DataValidate";
+$setup[0]->{cmd}     = "Nxyter-0xfe49-DataValidate";
 $setup[0]->{period}  = -1;
 $setup[0]->{address} = 1;
 
-$setup[1]->{name}    = "TrigValid";
-$setup[1]->{cmd}     = "Nxyter-0x3800-TriggerValidate";
+$setup[1]->{name}    = "TrigValidate";
+$setup[1]->{cmd}     = "Nxyter-0xfe49-TriggerValidate";
 $setup[1]->{period}  = -1;
 $setup[1]->{address} = 1;
 
 $setup[2]->{name}    = "TriggerHandler";
-$setup[2]->{cmd}     = "Nxyter-0x3800-TriggerHandler";
+$setup[2]->{cmd}     = "Nxyter-0xfe49-TriggerHandler";
 $setup[2]->{period}  = -1;
 $setup[2]->{address} = 1;
 
 $setup[3]->{name}    = "Testpulse";
-$setup[3]->{cmd}     = "Nxyter-0x3800-TestPulse";
+$setup[3]->{cmd}     = "Nxyter-0xfe49-TestPulse";
 $setup[3]->{period}  = -1;
 $setup[3]->{address} = 1;
 
 $setup[4]->{name}    = "DataReceiver";
-$setup[4]->{cmd}     = "Nxyter-0x3800-DataReceiver";
+$setup[4]->{cmd}     = "Nxyter-0xfe49-DataReceiver";
 $setup[4]->{period}  = -1;
 $setup[4]->{address} = 1;
 
 $setup[5]->{name}    = "I2CRegister";
-$setup[5]->{cmd}     = "Nxyter-0x3800-NxyterI2C";
+$setup[5]->{cmd}     = "Nxyter-0xfe49-NxyterI2C";
 $setup[5]->{period}  = -1;
 $setup[5]->{address} = 1;
 
 $setup[6]->{name}    = "DACRegister";
-$setup[6]->{cmd}     = "Nxyter-0x3800-NxyterDAC";
+$setup[6]->{cmd}     = "Nxyter-0xfe49-NxyterDAC";
 $setup[6]->{period}  = -1;
 $setup[6]->{address} = 1;
 
 
 $setup[7]->{name}    = "OtherStuff";
-$setup[7]->{cmd}     = "Nxyter-0x3800-TriggerValidate";
+$setup[7]->{cmd}     = "Nxyter-0xfe49-TriggerValidate";
 $setup[7]->{period}  = -1;
 $setup[7]->{generic} = 1;
 
