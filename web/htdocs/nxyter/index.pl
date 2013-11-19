47c311d3d1aa678f26a3f074e18b22aac2b28bed
Ludwig Maier <lmaier@bia.e12.ph.tum.de>
2013-11-19 18:41:13 +0100

    nxyter register updates

--- web/htdocs/nxyter/index.pl
+++ web/htdocs/nxyter/index.pl
@@ -27,35 +27,29 @@ $setup[1]->{period}  = -1;
 $setup[1]->{address} = 1;
 
 $setup[2]->{name}    = "TriggerHandler";
-$setup[2]->{cmd}     = "Nxyter-0xfe49-TriggerHandler";
+$setup[2]->{cmd}     = "Nxyter-0xfe49-TriggerHandler&Nxyter-0xfe49-TestPulse";
 $setup[2]->{period}  = -1;
 $setup[2]->{address} = 1;
 
-$setup[3]->{name}    = "Testpulse";
-$setup[3]->{cmd}     = "Nxyter-0xfe49-TestPulse";
+$setup[3]->{name}    = "DataReceiver";
+$setup[3]->{cmd}     = "Nxyter-0xfe49-DataReceiver";
 $setup[3]->{period}  = -1;
 $setup[3]->{address} = 1;
 
-$setup[4]->{name}    = "DataReceiver";
-$setup[4]->{cmd}     = "Nxyter-0xfe49-DataReceiver";
+$setup[4]->{name}    = "I2CRegister";
+$setup[4]->{cmd}     = "Nxyter-0xfe49-NxyterI2C";
 $setup[4]->{period}  = -1;
 $setup[4]->{address} = 1;
 
-$setup[5]->{name}    = "I2CRegister";
-$setup[5]->{cmd}     = "Nxyter-0xfe49-NxyterI2C";
+$setup[5]->{name}    = "DACRegister";
+$setup[5]->{cmd}     = "Nxyter-0xfe49-NxyterDAC";
 $setup[5]->{period}  = -1;
 $setup[5]->{address} = 1;
 
-$setup[6]->{name}    = "DACRegister";
-$setup[6]->{cmd}     = "Nxyter-0xfe49-NxyterDAC";
+$setup[6]->{name}    = "NXControl";
+$setup[6]->{cmd}     = "Nxyter-0xfe49-NXControl";
 $setup[6]->{period}  = -1;
-$setup[6]->{address} = 1;
-
-
-$setup[7]->{name}    = "OtherStuff";
-$setup[7]->{cmd}     = "Nxyter-0xfe49-TriggerValidate";
-$setup[7]->{period}  = -1;
-$setup[7]->{generic} = 1;
+$setup[6]->{generic} = 1;
 
 xmlpage::initPage(\@setup,$page);
 
