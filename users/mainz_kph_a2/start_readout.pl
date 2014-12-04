a8596065b71d871a16f17eac0b27faf5165870ce
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-12-04 14:35:25 +0100

    Users: A2: Use default port 50000 for readout

--- users/mainz_kph_a2/start_readout.pl
+++ users/mainz_kph_a2/start_readout.pl
@@ -16,5 +16,5 @@ exit if defined $ARGV[0];
 $c=q|daq_evtbuild -m 1 -d file -o /hldfiles|;
 system("$c &");
 sleep 2;
-$c=q|daq_netmem -m 1 -i UDP:192.168.1.1:50001|;
+$c=q|daq_netmem -m 1 -i UDP:192.168.1.1:50000|;
 system("$c &");
