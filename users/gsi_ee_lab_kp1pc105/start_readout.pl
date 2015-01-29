1ac69776a205391d9b0aae51f070c26bfa72fe71
HADES DAQ <hadaq@kp1pc105.gsi.de>
2015-01-29 11:06:11 +0100

    new boards, mt

--- users/gsi_ee_lab_kp1pc105/start_readout.pl
+++ users/gsi_ee_lab_kp1pc105/start_readout.pl
@@ -15,5 +15,5 @@ qx($c);
 $c=q|xterm -geometry 122x14-0+0 -e bash -c 'daq_evtbuild -m 1 -d file -o /tmp'|;
 system("$c &");
 sleep 1;
-$c="xterm -geometry 82x10-0+210 -e bash -c 'daq_netmem -m 1 -i UDP:127.0.0.1:50002'";
+$c="xterm -geometry 82x10-0+210 -e bash -c 'daq_netmem -m 1 -i UDP:127.0.0.1:50001'";
 system("$c &");
