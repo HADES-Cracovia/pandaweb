d707131287763b278d67b14d961764b3a29843b9
Jan Michel <j.michel@gsi.de>
2015-04-01 17:27:17 +0200

    updated registers of MVD roc

--- web/htdocs/mvd/roc.pl
+++ web/htdocs/mvd/roc.pl
@@ -25,17 +25,17 @@ $page->{link}  = "../";
 my @setup;
 $setup[0]->{name}    = "RocStatus";
 $setup[0]->{cmd}     = "Mvd-0xfe4d-RocStatus";
-$setup[0]->{period}  = 5000;
+$setup[0]->{period}  = 1000;
 $setup[0]->{address} = 1;
 
 $setup[1]->{name}    = "RocStatistics";
 $setup[1]->{cmd}     = "Mvd-0xfe4d-RocStatistics";
-$setup[1]->{period}  = 5000;
+$setup[1]->{period}  = 1000;
 $setup[1]->{address} = 1;
 
-$setup[2]->{name}    = "RocStatus";
-$setup[2]->{cmd}     = "Mvd-0xfe4d-RocStatus";
-$setup[2]->{period}  = -1;
+$setup[2]->{name}    = "ClusterFinder";
+$setup[2]->{cmd}     = "Mvd-0xfe4d-ClusterFinder";
+$setup[2]->{period}  = 1000;
 $setup[2]->{generic} = 1;
 
 xmlpage::initPage(\@setup,$page);
