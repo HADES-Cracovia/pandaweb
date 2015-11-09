c130cd2f4dacbc2cdbd44bde94ad4e2bc9d7f0b6
Jan Michel <j.michel@gsi.de>
2015-11-09 11:34:22 +0100

    adding input monitor and trigger generation registers for Trb3sc

--- web/htdocs/tdc/inputmonitor.pl
+++ web/htdocs/tdc/inputmonitor.pl
@@ -42,6 +42,24 @@ $setup[2]->{period}  = 1000;
 $setup[2]->{address} = 1;
 $setup[2]->{rate}    = 1;
 
+$setup[3]->{name}    = "Trb3scMonitorCtrl";
+$setup[3]->{cmd}     = "InputMonitorTrb3sc-0xfe60-MonitorRegs";
+$setup[3]->{period}  = 1000;
+$setup[3]->{address} = 1;
+$setup[3]->{rate}    = 1;
+
+$setup[4]->{name}    = "Trb3scMonitorCounters";
+$setup[4]->{cmd}     = "InputMonitorTrb3sc-0xfe60-MonitorCounters";
+$setup[4]->{period}  = 1000;
+$setup[4]->{address} = 1;
+$setup[4]->{rate}    = 1;
+
+$setup[5]->{name}    = "Trb3scTrigger";
+$setup[5]->{cmd}     = "InputMonitorTrb3sc-0xfe60-Trigger";
+$setup[5]->{period}  = 1000;
+$setup[5]->{address} = 1;
+$setup[5]->{rate}    = 1;
+
 
 xmlpage::initPage(\@setup,$page);
  
