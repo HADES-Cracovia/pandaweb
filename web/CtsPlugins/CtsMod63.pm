86fbb27b2efefe736b774b1ec1438b708c3d532b
Jan Michel <j.michel@gsi.de>
2016-07-25 16:23:30 +0200

    add CTS ETM module for Mimosa26

--- web/CtsPlugins/CtsMod63.pm
+++ web/CtsPlugins/CtsMod63.pm
@@ -1,4 +1,4 @@
-# Module: CBM DLM ETM
+# Module: Mimosa26 External Trigger Module
 # 
 
 package CtsMod63;
@@ -8,10 +8,9 @@ package CtsMod63;
 use warnings;
 use strict;
 
-sub moduleName {"M26 MVD ETM"}
+sub moduleName {"Mimosa26"}
 
 sub init {
-return;
    my $self    = $_[0];
    my $address = $_[1];
    
@@ -23,6 +22,23 @@ return;
 
    my $header = $self->{'_cts'}{'_enum'}{0x63}->read();
    
+   $regs->{'trg_m26_status'} = new TrbRegister($address + 1, $trb, {
+         'value' => {'lower' =>  0, 'len' => 32, 'type' => 'hex'},
+      }, {
+         'accessmode' => "ro",
+         'label' => "Mimosa26 Status Register",
+         'monitor' => '1'
+      });
+
+   $regs->{'trg_m26_control'} = new TrbRegister($address + 2, $trb, {},
+      {
+         'accessmode' => "rw",
+         'label' => "Mimosa26 Control Register",
+         'monitor' => '1',
+         'export' => 1
+      });
+
+   $self->{'_cts'}->getProperties->{'itc_assignments'}[$header->{'itc_base'}] = "External Trigger - Mimosa26";
 }
 
 1;
