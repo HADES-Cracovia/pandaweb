02755869b9af912c78788c59920c30e96d2670ff
HADES DAQ <hadaq@hadesp43.gsi.de>
2017-01-26 15:07:25 +0100

    current status, mt

--- users/gsi_rich/dmon_config.pl
+++ users/gsi_rich/dmon_config.pl
@@ -3,98 +3,34 @@
 
 #Scripts to start & order of icons in the Overview
 activeScripts => [['time','ping','-','-','daqop'],
-                  ['numfee','temperature','reftime','billboard','mbs'],
-                  ['beamintensity','beammonitors','-','richhvres','richenvironment'],
+                  ['numfee','temperature','reftime','',''],
+                  ['','','-','',''],
                   ['trgrate','datarate','deadtime','-','-'],
-                  ['heatmaprich','padiwatemp','padiwaonline','currents','pscurrents'],
-                  ['cbmnetlink','cbmnetdata','cbmnetsync','-','-']],
+                  ['','','','',''],
+                  ['','','','-','-']],
                   
 #Names to be shown in left column of Overview (max 4 letters!)
-qaNames => ['sys','main','beam','rate','Pdwa','CNet','-'],                  
+qaNames => ['sys','main','beam','rate','pwr','-','-'],                  
 
 #Expected number of FPGAs in system
-NumberOfFpga => 90,
+NumberOfFpga => 7,
 
 #The address of the individual boards
-CtsAddress   => 0x7005,   
-PadiwaBroadcastAddress => 0xfe4c,
+CtsAddress   => 0xc001,   
 
-PadiwaTrbAdresses => [0x0010,0x0011,0x0012,0x0013,
-                      0x0020,0x0021,0x0022,0x0023,
-                      0x0030,0x0031,0x0032,0x0033,
-                      0x0040,0x0041,0x0042,0x0043,
-                      0x0050,0x0051,0x0052,0x0053,
-                      0x0060,0x0061,0x0062,0x0063,
-                      0x0070,0x0071,0x0072,0x0073,
-                      0x0080,0x0081,0x0082,0x0083,
-                      0x0090,0x0091,0x0092,0x0093,
-                      0x00a0,0x00a1,0x00a2,0x00a3,
-                      0x00b0,0x00b1,0x00b2,0x00b3,
-                      0x00c0,0x00c1,0x00c2,0x00c3,
-                      0x00d0,0x00d1,0x00d2,0x00d3,
-                      0x00e0,0x00e1,0x00e2,0x00e3,
-                      0x00f0,0x00f1,0x00f2,0x00f3,
-                      0x0100,0x0101,0x0102,0x0103,
-                      0x0111,0x0113],
+HubTrbAdresses =>  [0xfffe],
 
-HubTrbAdresses =>  [0x7005,0x7000,0x7001,0x7002,0x7003,
-                    0x0015,
-                    0x0025,
-                    0x0035,
-                    0x0045,
-                    0x0055,
-                    0x0065,
-                    0x0075,
-                    0x0085,
-                    0x0095,
-                    0x00a5,
-                    0x00b5,
-                    0x00c5,
-                    0x00d5,
-                    0x00e5,
-                    0x00f5,
-                    0x0105,
-                    0x0115],
-
-BillboardAddress => 0x0112,
-MBSAddress => 0x0112,
                     
 #Addresses of all TDCs. Either single or broadcast addresses
-TdcAddress   => [0xfe4c,0xfe4e,0x7005],  
+TdcAddress   => [0xfe51],  
 
 #IPs of all devices which should react on a ping
 TrbIP => [
-"192.168.0.29",
-    "192.168.0.47",
-    "192.168.0.56",
-#           "192.168.0.57",#extra TRB3
-    "192.168.0.59",
-    "192.168.0.72",
-    "192.168.0.73",
-    "192.168.0.74",
-    "192.168.0.78",
-    "192.168.0.83",
-    "192.168.0.84",
-    "192.168.0.89",
-    "192.168.0.97",
-    "192.168.0.99",
-    "192.168.0.101",
-    "192.168.0.102",
-    "192.168.0.104",
-    "192.168.0.105"
+"192.168.102.165",
 ],
 
-#Channel to read spill intensity from. Give limit for off-spill detection
-BeamTRB => 0x0110,
-BeamChan => 0xc005,
-SpillThreshold => 30,
-
-#Name detectors 
-BeamDetectorsTrb  => [0x0110, 0x0110, 0x0111,0x0110,0x0111,0x0110,0x0113,0x0111,0x0110],
-BeamDetectorsChan => [0xc001, 0xc003, 0xc001,0xc009,0xc005,0xc00b,0xc009,0xc009,0xc005],
-BeamDetectorsName => ['Fngr_d', 'Lead_d', 'C1',  'C1_d', 'C2',  'C2_d', 'Lead1', 'Lead2', 'Hodo'],
 
 #User directory
-UserDirectory => '/home/hadaq/trbsoft/daqtools/users/cern_cbmrich/',
-PowerSupScript => 'measure_powers.sh' # relative to user dir
+UserDirectory => '/home/hadaq/trbsoft/daqtools/users/gsi_rich/',
+# PowerSupScript => 'measure_powers.sh' # relative to user dir
 
